ffe1db5308a34619553dfff737b8d44d5e6cf5cf
searchfresh <114284569+searchfresh@users.noreply.github.com>
2024-06-13 17:47:32 +0800

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,3 @@
-# from model.LKUnet_lr.model_LKUNet_lr_ocr_psd import LKUNet as LKUNet  # LKUnet_lr 版本
+from model.LKUnet_lr.model_LKUNet_lr_ocr_psd import LKUNet as LKUNet2  #二， LKUnet_lr 版本 权重69
 # from model.LKUNet.model_LKUNet import LKUNet as LKUNet  # 6月10号修改版本
-from model.LKUNet.model_LKUNet_lk import LKUNet as LKUNet  # 6月12号修改版本
+from model.LKUNet.model_LKUNet_lk import LKUNet as LKUNet1  # 6月12号修改版本   #一， 目前正在跑的版本
