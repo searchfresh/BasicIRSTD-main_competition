cc79c12219a170f9ae00da6b408875430b6b2790
searchfresh <1339521252@qq.com>
2024-06-19 18:01:52 +0800

    Update filter_process.py

--- filter_process.py
+++ filter_process.py
@@ -6,7 +6,8 @@ def filter_large(image):
         image = image[0,0,:,:]
     numpy_array = image.numpy()
     binary_image = numpy_array.astype(np.uint8) * 255
-    contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours= cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
     # 创建一个和输入图像大小相同的空白图像
     output_image = np.zeros_like(binary_image)
     if len(contours)<10:
@@ -18,7 +19,7 @@ def filter_large(image):
             area = cv2.contourArea(contour)
             if len(approx) <= 5 and area>=14 and len(approx) >= 4:
                 cv2.drawContours(output_image, [contour], -1, 255, -1)
-        output_image = (torch.from_numpy(np.ascontiguousarray(output_image))>0).float()
+        output_image = (torch.from_numpy(output_image)>0).float()
     if len(output_image.shape) == 2:
         output_image = output_image.unsqueeze(0).unsqueeze(0)
     return output_image
